dda37fb88115c44e3d6e4026555e62766e3a3e1a
Kyle Faith <84872911+kfaith@users.noreply.github.com>
2021-06-22 23:28:46 -0500

    Add files via upload

---
+++ KyleFaith_FinalProgram/KyleFaith_Final/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pictBox = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pictBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pictBox
+        '
+        Me.pictBox.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pictBox.Location = New System.Drawing.Point(0, 0)
+        Me.pictBox.Name = "pictBox"
+        Me.pictBox.Size = New System.Drawing.Size(549, 334)
+        Me.pictBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.pictBox.TabIndex = 0
+        Me.pictBox.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(549, 334)
+        Me.Controls.Add(Me.pictBox)
+        Me.Name = "Form1"
+        Me.Text = "KyleFaith_Final"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.pictBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pictBox As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ KyleFaith_FinalProgram/KyleFaith_Final/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Dim randomNum As New Random()
+    Dim x, y, xPos, yPos As Integer
+    Dim gr As Graphics
+    Dim randColor As Color
+    Dim myRectangle As Rectangle
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        'Chooses a random color and opacity
+        Dim randBrush As New SolidBrush(Color.FromArgb(randomNum.Next(1, 255), randomNum.Next(1, 255), randomNum.Next(1, 255), randomNum.Next(1, 255)))
+
+        'Chooses a random size between 30 and 80.
+        x = randomNum.Next(30, 80)
+        y = randomNum.Next(30, 80)
+        'Chooses a random position(the top left corner) for the rectangle.
+        xPos = randomNum.Next(1, Screen.PrimaryScreen.Bounds.Width - x)
+        yPos = randomNum.Next(1, Screen.PrimaryScreen.Bounds.Height - y)
+
+        gr = pictBox.CreateGraphics
+        'Makes a rectangle with the random color, position, and size.
+        gr.FillRectangle(randBrush, xPos, yPos, x, y)
+
+    End Sub
+
+
+
+
+    Private Sub pictBox_Click(sender As Object, e As EventArgs) Handles pictBox.Click
+
+        'If the timer isnt running when left clicked, start timer. If timer is running when left clicked, stop timer.
+        If Timer1.Enabled = False Then
+
+            Timer1.Enabled = True
+
+        ElseIf (Timer1.Enabled = True) Then
+
+            Timer1.Enabled = False
+
+        End If
+
+    End Sub
+
+
+End Class
